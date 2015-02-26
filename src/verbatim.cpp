e1f9e9b7a3ce30a59944ff8a331573b04758380e
Jim Vanns <james.vanns@gmail.com>
2015-02-26 09:54:09 +0000

    src/verbatim: Introduce the main verbatim program
    Small building blocks...

---
+++ src/verbatim.cpp
@@ -0,0 +1,34 @@
+/*
+ * vim: set smartindent autoindent expandtab tabstop=4 shiftwidth=4:
+ */
+
+// verbatim
+#include "Database.hpp"
+
+// libstdc++
+#include <iostream>
+
+using std::cout;
+using std::cerr;
+using std::endl;
+
+namespace {
+
+} // anonymous
+
+int main(int argc, char *argv[])
+{
+    using verbatim::Traverse;
+    using verbatim::Database;
+
+    if (!argv[1]) {
+        cerr << "Provide a source 'root' path" << endl;
+        return 1;
+    }
+
+    Traverse t;
+    Database db(t);
+
+    return 0;
+}
+
