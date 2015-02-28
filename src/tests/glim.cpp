31697ea42fe1c4253d428ad1e6b0b6528c96e82b
Jim Vanns <james.vanns@gmail.com>
2015-02-28 09:04:05 +0000

    src/tests/glim: Add a few more tests and comment them all

--- src/tests/glim.cpp
+++ src/tests/glim.cpp
@@ -13,6 +13,9 @@
 
 int main(int argc, char *argv[])
 {
+    /*
+     * Test std::string KVP insert and retrieval
+     */
     {
         glim::Mdb db("/tmp/lmdb-test.db", 4, "lmdb-test", 0, true, 0600);
         std::string key("AKEY"), in("A Value"), out;
@@ -22,6 +25,9 @@ int main(int argc, char *argv[])
         assert(in == out);
     }
 
+    /*
+     * Test integer KVP insert and retrieval
+     */
     {
         glim::Mdb db("/tmp/lmdb-test.db", 4, "lmdb-test", 0, true, 0600);
         unsigned int key = 0xDEADBEEF, out;
@@ -30,6 +36,37 @@ int main(int argc, char *argv[])
         db.add(key, in);
         assert(db.first(key, out));
         assert(in == out);
+    }
+
+    /*
+     * Test std::string retrieval (disk persistance)
+     */
+    {
+        glim::Mdb db("/tmp/lmdb-test.db", 4, "lmdb-test", 0, true, 0600);
+        std::string key("AKEY"), expected("A Value"), value;
+
+        assert(db.first(key, value));
+        assert(value == expected);
+    }
+
+    /*
+     * Test key removal
+     */
+    {
+        glim::Mdb db("/tmp/lmdb-test.db", 4, "lmdb-test", 0, true, 0600);
+        const unsigned int key = 0xDEADBEEF;
+
+        assert(db.erase(key));
+    }
+
+    /*
+     * Test key absence (disk persistance)
+     */
+    {
+        glim::Mdb db("/tmp/lmdb-test.db", 4, "lmdb-test", 0, true, 0600);
+        unsigned int key = 0xDEADBEEF, out;
+
+        assert(!db.first(key, out));
     }
 
     return 0;
