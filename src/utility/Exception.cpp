539e94907b5a97d6d29e701a484be9c05e2e7cf8
Jim Vanns <james.vanns@gmail.com>
2015-03-04 08:18:45 +0000

    src/utility/Exception: Add a set of Exception classes

---
+++ src/utility/Exception.cpp
@@ -0,0 +1,122 @@
+/*
+ * vim: set smartindent autoindent expandtab tabstop=4 shiftwidth=4:
+ */
+
+// Interface
+#include "Exception.hpp"
+
+// libc
+#include <stdio.h> // For vsnprintf
+#include <string.h> // For strerror
+#include <stdint.h> // For uint16_t
+
+// libstdc++
+using std::string;
+
+namespace {
+
+inline
+string
+default_errno2str(const int error)
+{
+    return error == 0 ? "" : strerror(error);
+}
+
+} // anonymous
+
+namespace verbatim {
+namespace utility {
+
+Exception::Exception() : error(0),
+                         message("Exception thrown from "),
+                         errno2str(default_errno2str)
+{
+}
+
+Exception::Exception(const Exception &e)
+{
+    *this = e;
+}
+
+Exception&
+Exception::operator= (const Exception &e)
+{
+    if (this != &e) {
+        error = e.error;
+        message = e.message;
+    }
+
+    return *this;
+}
+
+void
+Exception::init(const string &from, int e, const char *format)
+{
+    static const uint16_t buffer_size = 1024;
+    char buffer[buffer_size];
+
+    memset(buffer, '\0', buffer_size);
+    vsnprintf(buffer, buffer_size, format, args);
+    va_end(args);
+
+    message += (from + ":\n** " + buffer);
+
+    if ((error = e) && errno2str) {
+        const string s(errno2str(e));
+
+        if (!s.empty())
+            message += "\n** External 'strerror' function suggests: " + s;
+    }
+}
+
+// Exceptions
+LockError::LockError(const string &from, int error, const char *format, ...)
+{
+    va_start(args, format);
+    init(from, error, format); // va_end happens in here
+}
+
+FileError::FileError(const string &from, int error, const char *format, ...)
+{
+    va_start(args, format);
+    init(from, error, format); // va_end happens in here
+}
+
+LogicError::LogicError(const string &from, int error, const char *format, ...)
+{
+    va_start(args, format);
+    init(from, error, format); // va_end happens in here
+}
+
+ThreadError::ThreadError(const string &from, int error, const char *format, ...)
+{
+    va_start(args, format);
+    init(from, error, format); // va_end happens in here
+}
+
+SignalError::SignalError(const string &from, int error, const char *format, ...)
+{
+    va_start(args, format);
+    init(from, error, format); // va_end happens in here
+}
+
+ProcessError::ProcessError(const string &from,
+                           int error,
+                           const char *format,
+                           ...)
+{
+    va_start(args, format);
+    init(from, error, format); // va_end happens in here
+}
+
+ConversionError::ConversionError(const string &from,
+                                 int error,
+                                 const char *format,
+                                 ...)
+{
+    va_start(args, format);
+    init(from, error, format); // va_end happens in here
+}
+
+} // utility
+} // verbatim
---
+++ src/utility/Exception.hpp
@@ -0,0 +1,80 @@
+/*
+ * vim: set smartindent autoindent expandtab tabstop=4 shiftwidth=4:
+ */
+
+#ifndef VERBATIM_UTILITY_EXCEPTION_HPP
+#define VERBATIM_UTILITY_EXCEPTION_HPP
+
+// libstdc++
+#include <string>
+#include <exception>
+
+// libc
+#include <cstdarg> // For va_list et al
+
+namespace verbatim {
+namespace utility {
+
+class Exception : public std::exception
+{
+    public:
+        /* Member functions/methods */
+        Exception(const Exception &e);
+        Exception& operator= (const Exception &e);
+
+        const char* what() const throw() { return message.c_str(); }
+    protected:
+        /* Member variables/attributes */
+        int error; // Copy of errno
+        va_list args; // The argument list
+        std::string message; // The resulting error message
+        std::string (*errno2str)(int error); // strerror() etc.
+
+        /* Member functions/methods */
+        Exception();
+        virtual ~Exception() throw() {}
+
+        void init(const std::string &from, int error, const char *format);
+};
+
+// Exceptions
+struct LockError : public Exception
+{
+    LockError(const std::string &from, int error, const char *format, ...);
+};
+
+struct FileError : public Exception
+{
+    FileError(const std::string &from, int error, const char *format, ...);
+};
+
+struct LogicError : public Exception
+{
+    LogicError(const std::string &from, int error, const char *format, ...);
+};
+
+struct ThreadError : public Exception
+{
+    ThreadError(const std::string &from, int error, const char *format, ...);
+};
+
+struct SignalError : public Exception
+{
+    SignalError(const std::string &from, int error, const char *format, ...);
+};
+
+struct ProcessError : public Exception
+{
+    ProcessError(const std::string &from, int error, const char *format, ...);
+};
+
+struct ConversionError : public Exception
+{
+    ConversionError(const std::string &from, int error, const char *format,...);
+};
+
+} // utility
+} // verbatim
+
+#endif
+
