372bd9f140a7831dffb10e1c182d698274c703a7
Jim Vanns <james.vanns@gmail.com>
2015-03-04 08:29:14 +0000

    src/verbatim: Add some proper option/argument parsing and a usage function

--- src/verbatim.cpp
+++ src/verbatim.cpp
@@ -4,38 +4,103 @@
 
 // verbatim
 #include "Database.hpp"
+#include "utility/tools.hpp"
 
 // libstdc++
 #include <iostream>
+#include <exception>
 
+// libc
+#include <getopt.h>
+
+// libstdc++
 using std::cout;
 using std::cerr;
 using std::endl;
+using std::exception;
+
+// verbatim
+using verbatim::Traverse;
+using verbatim::Database;
+using verbatim::utility::str2int;
 
 namespace {
+
+void
+print_usage(const char *program_name)
+{
+    cerr << "usage: "
+         << program_name << " [options] " << "<db file> <music directory>\n\n";
+    cerr << "Options (defaults in parenthesis):\n"
+         << "-h/--help             "
+         << "Print this help message you're reading, then terminate\n"
+         << "-v/--verbose          "
+         << "Print noisy verbose messages to stdout (false)\n"
+         << "-c/--concurrency <N>  "
+         << "No. of worker threads to run in parallel (2)\n";
+}
 
 } // anonymous
 
 int main(int argc, char *argv[])
 {
-    using verbatim::Traverse;
-    using verbatim::Database;
-
-    if (!argv[1]) {
-        cerr << "Provide a filename in which to store data" << endl;
+    if (argc < 2) {
+        print_usage(argv[0]);
         return 1;
     }
 
-    if (!argv[2]) {
-        cerr << "Provide a source 'root' path" << endl;
+    /*
+     * Default values for optional flags - read help message in print_usage()!
+     */
+    bool verbose = false;
+    uint16_t threads = 2;
+    char *db_path = NULL, *music_path = NULL;
+
+    try {
+        int option_index, c = 0;
+        const char *short_options = "+hvc:";
+        struct option const long_options[] = {
+            {"help", 0, NULL, 'h'},
+            {"verbose", 0, NULL, 'v'},
+            {"concurrency", 1, NULL, 'c'},
+            {NULL, 0, NULL, 0}
+        };
+
+        do {
+            c = getopt_long(argc,argv,short_options,long_options,&option_index);
+
+            switch (c) {
+                case 'v':
+                    verbose = true;
+                    break;
+                case 'c': {
+                        static const uint16_t min = 1, max = 256;
+                        threads = str2int<uint16_t>(optarg, &min, &max);
+                    }
+                    break;
+                case 'h':
+                    print_usage(argv[0]);
+                    return 1;
+            }
+        } while (c != -1);
+    } catch (const exception &e) {
+        cerr << e.what() << endl;
+        return 1;
+    }
+
+    if ((argc - optind) < 2) {
+        print_usage(argv[0]);
         return 1;
     }
 
     Traverse t;
     Database db(t);
 
-    db.open(argv[1]);
-    t.scan(argv[2]);
+    db_path = argv[optind++];
+    music_path = argv[optind];
+
+    db.open(db_path);
+    t.scan(music_path);
 
     return 0;
 }
