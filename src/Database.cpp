d812ccd2798edbdaac98debac9121e0c177b7dfc
Jim Vanns <james.vanns@gmail.com>
2015-03-11 08:44:47 +0000

    src/Database: Embed the album art into the Tag Database too
    This is the simple patch; it will duplicate the image for ever file that
    has the same artwork (eg. every MP3 track for an album). The next challenge
    is to dedupe it.

--- src/Database.cpp
+++ src/Database.cpp
@@ -12,6 +12,8 @@
 // Taglib
 #include <taglib/tag.h>
 #include <taglib/fileref.h>
+#include <taglib/id3v2tag.h>
+#include <taglib/attachedpictureframe.h>
 
 // libc
 #include <assert.h>
@@ -20,6 +22,37 @@ using std::string;
 using std::ostream;
 
 namespace {
+
+void
+add_image(const TagLib::Tag *source,
+          verbatim::Tag &destination)
+{
+    const TagLib::ID3v2::Tag *tag =
+        dynamic_cast<const TagLib::ID3v2::Tag*>(source);
+
+    if (!tag)
+        return; /* Not an ID3v2 tag. Perhaps an OGG or FLAC? */
+
+    const TagLib::ID3v2::FrameList &frames = tag->frameList("APIC");
+
+    if (frames.isEmpty())
+        return;
+
+    /*
+     * Adds only the first in the list and this assumes
+     * it is the album cover, not the back or inside etc.
+     */
+    TagLib::ID3v2::AttachedPictureFrame *frame =
+        static_cast<TagLib::ID3v2::AttachedPictureFrame*>(frames.front());
+
+    const size_t len = frame->picture().size();
+    const char *data = frame->picture().data();
+    verbatim::Tag::ByteVector::iterator to(destination.album_art.begin());
+
+    destination.album_art.reserve(len);
+    copy(data, data + len, to);
+    destination.mimetype = frame->mimeType().to8Bit();
+}
 
 struct GrabTag {
     GrabTag(glim::Mdb &d,
@@ -52,6 +85,8 @@ struct GrabTag {
         v.album = tags->album().to8Bit();
         v.title = tags->title().to8Bit();
         v.artist = tags->artist().to8Bit();
+
+        add_image(tags, v);
 
         db.add(k, v);
     }
