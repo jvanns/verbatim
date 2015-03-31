403d9ba1cf32a07ee3006dbed3328c7169f37610
Jim Vanns <james.vanns@gmail.com>
2015-03-31 11:24:42 +0100

    build.sh: Update build script to land us in a build sandbox

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
-set -eu
+set -eux
 
-exec docker build -t verbatim .
+ME="`readlink -f "$0"`"
+DIR="`dirname "$ME"`"
+NAME="`basename "$DIR"`"
+
+docker build -t "$NAME" .
+
+exec docker run \
+   -i \
+   -t \
+   --rm \
+   --net=host \
+   -v "${DIR}:/usr/src/${NAME}" \
+   "$NAME" bash -l
